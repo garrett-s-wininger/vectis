3a1d7ce8e95c8e1e9446dca926e0f6fe5bed2cc5
Garrett Wininger <garrett.s.wininger@outlook.com>
2026-05-29 23:59:52 -0400

    fix: streamline storybook

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -2,6 +2,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
+  features: {
+    sidebarOnboardingChecklist: false
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {}
